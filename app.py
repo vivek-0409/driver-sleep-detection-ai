4d7a594566fb03e08560dd00f2b38d2bd0370428
vivek-0409 <parekhvivek0409@gmail.com>
2026-06-03 23:25:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st
-import cv2
-import mediapipe as mp
+# import cv2
+# import mediapipe as mp
 import numpy as np
 from streamlit_webrtc import webrtc_streamer
 
 
+import streamlit as st
 
+st.write("STARTED")
+
+# import cv2
+# import mediapipe as mp
 # -----------------------------
 # MediaPipe Setup
 # -----------------------------
