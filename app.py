8a73ee0c7c5e87fd5f3583e676452d20fa4c68db
vivek-0409 <parekhvivek0409@gmail.com>
2026-06-03 22:36:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,18 +142,15 @@ def generate_frames():
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
         )
 
-@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/video')
 def video():
     return Response(
         generate_frames(),
         mimetype='multipart/x-mixed-replace; boundary=frame'
     )
 
-@app.route('/status')
 def status():
     return jsonify({
         "percentage": sleep_percentage,
